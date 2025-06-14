3c2450b15542446b6d2cb51fae53096a97ad27e4
degoliap <degoliap@gmail.com>
2025-06-15 01:41:27 +1200

    rep changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
+  base: '/echotip.v2/',
   css: {
     preprocessorOptions: {
       scss: {
